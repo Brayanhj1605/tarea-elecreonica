3194b071466723eaabe90c7249d71e197c3cdf34
Brayan Hernandez <>
2021-05-20 15:33:18 +0000

    terminado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
+using namespace std;
+bool comprobador;
+float numero1;
+float numero2;
 int main() {
-  std::cout << "Hello World!\n";
-}
+   cout <<"insertar numero1: ";
+   cin >>numero1;
+   cout <<"insertar numero2: ";
+   cin >>numero2;
+   if(numero1<numero2)
+   cout<<numero1<<"<"<<numero2<<endl;
+   if(numero1>numero2)
+   cout<<numero1<<">"<<numero2<<endl;
+   if(numero1==numero2)
+   cout<<numero1<<"=="<<numero2<<endl;
+   }
